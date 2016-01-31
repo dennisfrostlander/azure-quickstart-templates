34a59d83e79ec0969253225f7bbb4ab05e1ef3fe
Dennis Frostlander <dennis.frostlander@gmail.com>
2016-01-31 08:42:31 +0100

    added jre and teamcity

--- panda-setup/build-env.ps1
+++ panda-setup/build-env.ps1
@@ -1,4 +1,8 @@
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco install nodejs.install -y
+choco install jre8 -y
+choco install teamcity -y
 choco install googlechrome -y
+
+schtasks /create /tn "StartBuildAgent" /tr "c:\TeamCity\buildAgent\bin\agent.bat start" /sc onlogon /RL HIGHEST
