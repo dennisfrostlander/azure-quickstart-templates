7cb6a6a66e81bfd823cfc7ef01be947143741e64
Dennis Frostlander <dennis.frostlander@gmail.com>
2016-03-02 18:37:37 +0100

    added forced jre and sql server

--- panda-setup/build-env.ps1
+++ panda-setup/build-env.ps1
@@ -1,8 +1,10 @@
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco install nodejs.install -y
-choco install jre8 -y
 choco install teamcity -y
+choco install jre8 -y --force
 choco install googlechrome -y
+choco install mssqlserver2014express -y
+choco install MsSqlServerManagementStudio2014Express -y
 
 schtasks /create /tn "StartBuildAgent" /tr "c:\TeamCity\buildAgent\bin\agent.bat start" /sc onlogon /RL HIGHEST
